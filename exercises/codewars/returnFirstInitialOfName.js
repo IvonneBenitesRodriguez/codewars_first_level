5aca544efd47838fbe320730fb37c0c7644700fd
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-03-20 13:30:13 +0100

    Add exercise created

---
+++ exercises/codewars/returnFirstInitialOfName.js
@@ -0,0 +1,9 @@
+//The exercise is about returning the first initial
+// of a name and convert it to capital letters
+
+function abbrevName(name){
+    return name.charAt(0).toUpperCase() + ".";
+}
+console.log(abbrevName("mary"));
+
+
