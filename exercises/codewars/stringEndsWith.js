11dcf5bb39104f330a27cbadab35fecd6f161142
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-03-25 11:04:57 +0100

    Add stringEndWith codewars exercise

---
+++ exercises/codewars/stringEndsWith.js
@@ -0,0 +1,15 @@
+// Exercise : String ends with?
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//Examples:
+//Inputs: "abc", "bc"
+//Output: true
+//Inputs: "abc", "d"
+//Output: false
+//Solution:
+function solution(str,ending){
+    if(str.endsWith(ending)){
+        return true;
+    }else{
+        return false;
+    }
+}
