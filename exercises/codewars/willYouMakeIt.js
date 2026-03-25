1df1490e09977024e6cb528e54b27871f446b72c
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-03-25 11:05:28 +0100

    Add willYouMakeIt codewars exercise

---
+++ exercises/codewars/willYouMakeIt.js
@@ -0,0 +1,26 @@
+//Codewars exercise: Will you make it?
+//You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+//Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+//Function should return true if it is possible and false if not.
+
+// //Solution using arrow function:
+const zeroFuel = (distanceToPump, mpg, fuelLeft)=>{
+    if(fuelLeft * mpg < distanceToPump){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+console.log(zeroFuel(50, 25,50));
+
+//Solution using function :
+function zeroFuel(distanceToPump, mpg, fuelLeft){
+    if(fuelLeft * mpg < distanceToPump){
+        return false;
+    }else {
+        return true;
+    }
+};
+
+console.log(zeroFuel(50, 25,50));
