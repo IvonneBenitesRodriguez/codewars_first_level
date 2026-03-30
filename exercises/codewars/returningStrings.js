eb1b0036ba216115e2f42170ab8b2580afd384d0
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-03-30 15:29:45 +0200

    Add new codewars exercise named returning Strings

---
+++ exercises/codewars/returningStrings.js
@@ -0,0 +1,9 @@
+//Codewars exercise: Returning Strings
+function greet(name){
+  //your code here
+  let greeting = `"Hello, ${name} how are you doing today?"`;
+  return greeting;
+}
+console.log(greet("Pilar"));
+console.log(greet("Marius"));
+console.log(greet("Ryan"));
